5ff599581919cc765a0137c57d68084ae9c96186
Novout <novout@hotmail.com>
2026-03-08 21:49:50 -0300

    feat: utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
 export function random(arr: any[]) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
+
+export const format = (totalSeconds: number) => {
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+
+  const pad = (num: number) => num.toString().padStart(2, '0');
+
+  return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+}
